289016ed24fcd073107cccab6271fe7411c0918a
huytang99 <68282036+huytang99@users.noreply.github.com>
2022-03-08 00:45:33 +0700

    Add new forcast infor

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require("request");
 // Goal: Create a reusable function for getting the forecast
 //
 // 1. Setup the "forecast" function in utils/forecast.js
@@ -9,18 +9,32 @@ const request = require('request')
 //    - Success, pass forecast string for data (same format as from before)
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=02980751c7ef78c927e0513d551b66ea&query=' + latitude + ',' + longitude + '&units=m'
-  request({ url, json: true}, (error, { body }) => {
-    if(error) {
-      callback('Unable to connect to weather service!', undefined)
-    } else if(body.error) {
-      callback('Unable to find location!',undefined)
+  const url =
+    "http://api.weatherstack.com/current?access_key=02980751c7ef78c927e0513d551b66ea&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=m";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location!", undefined);
     } else {
-      const data = body.current
-      const {weather_descriptions, temperature, feelslike} = data
-      callback(undefined, weather_descriptions[0] + ". It is currently " + temperature + ' degrees out. It feels like ' + feelslike + ' degrees out.')
+      const data = body.current;
+      const { weather_descriptions, temperature, feelslike, humidity } = data;
+      callback(
+        undefined,
+        weather_descriptions[0] +
+          ". It is currently " +
+          temperature +
+          " degrees out. It feels like " +
+          feelslike +
+          " degrees out. The humidity is " +
+          humidity + "%."
+      );
     }
-  })
-} 
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
